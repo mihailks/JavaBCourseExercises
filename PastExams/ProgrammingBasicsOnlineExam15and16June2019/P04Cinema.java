3b2529f5686e7fce435ede89770f8e0e2f14d2c4
Mihail Slavov <mihail_slavov@icloud.com>
2022-07-04 20:21:00 +0300

    Update P04Cinema.java

--- PastExams/ProgrammingBasicsOnlineExam15and16June2019/P04Cinema.java
+++ PastExams/ProgrammingBasicsOnlineExam15and16June2019/P04Cinema.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 public class P04Cinema {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int currentPPL=0;
+        int income = 0;
         int n = Integer.parseInt(scanner.nextLine());
         String input = scanner.nextLine();
-        while (!input.equals("Movie time!")){
+        while (!input.equals("Movie time!")) {
             int people = Integer.parseInt(input);
-            currentPPL +=people;
-            if (n<=currentPPL){
+            if (n - people >= 0) {
+                n -= people;
+                income += people * 5;
+                if (people % 3 == 0) {
+                    income -= 5;
+                }
+            }
+            else {
                 System.out.println("The cinema is full.");
                 break;
             }
             input = scanner.nextLine();
         }
-        double totalPrice;
-        if (currentPPL%3==0){
-            totalPrice = currentPPL*5-5;
-        } else {
-            totalPrice = currentPPL*5;
+        if (input.equals("Movie time!")) {
+            System.out.printf("There are %d seats left in the cinema.%n", n);
         }
-        if (input.equals("Movie time")){
-            System.out.printf("There are %d seats left in the cinema.",n-currentPPL);
-        }
-        System.out.printf("Cinema income - %.2f lv.",totalPrice);
+        System.out.printf("Cinema income - %d lv.", income);
     }
 }
