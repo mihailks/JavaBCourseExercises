be707ce68241aab2e068c37868cee2b2110e68a5
Mihail Slavov <inj.slavov@gmail.com>
2022-07-12 10:27:51 +0300

    Update P08EqualPairs.java

--- PB - More Exercises/For-Loop - More Exercises/P08EqualPairs.java
+++ PB - More Exercises/For-Loop - More Exercises/P08EqualPairs.java
@@ -1,34 +1,33 @@
-package ForLoopMoreExercises;
-
 import java.util.Scanner;
 
 public class P08EqualPairs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
-        int first = 0;
-        int second = 0;
+        int diff = 0;
+        int maxDiff = 0;
+        int sum;
         int temp = 0;
-        int maxDiff = Integer.MIN_VALUE;
-        boolean isSame = true;
-
-
-        for (int i = 1; i <= n; i++) {
-            int a = Integer.parseInt(scanner.nextLine());
-            int b = Integer.parseInt(scanner.nextLine());
-
-            first = a + b;
-            if (second-first>maxDiff){
-                maxDiff = first - second;
+        for (int i = 0; i < n; i++) {
+            int num1 = Integer.parseInt(scanner.nextLine());
+            int num2 = Integer.parseInt(scanner.nextLine());
+            sum = num1 + num2;
+            if (i == 0) {
+                temp = sum;
+            } else {
+                if (temp != sum) {
+                    diff = Math.abs(sum - temp);
+                    temp = sum;
+                }
+                if (diff > maxDiff) {
+                    maxDiff = diff;
+                }
             }
-            second=first;
-
         }
-        if (maxDiff==0){
-            System.out.printf("Yes, value=%d",first);
+        if (diff == 0) {
+            System.out.printf("Yes, value=%d", temp);
         } else {
-            System.out.printf("No, maxdiff=%d",maxDiff);
+            System.out.printf("No, maxdiff=%d", maxDiff);
         }
-
     }
 }
